74376f49a4a3e583cd24c7b0d13a4a120e961b32
Carl Guo <carlguo866@gmail.com>
2023-09-17 18:27:19 -0400

    fix spirit

--- tourney/models/team.py
+++ tourney/models/team.py
@@ -2,9 +2,11 @@ import functools
 
 from django.core.exceptions import ValidationError
 from django.db import models
-from django_better_admin_arrayfield.models.fields  import ArrayField
+from django_better_admin_arrayfield.models.fields import ArrayField
 
 # @functools.total_ordering
+
+
 class Team(models.Model):
     user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, primary_key=True,
                                 related_name='team')
@@ -40,7 +42,8 @@ class Team(models.Model):
 
     def rounds(self):
         if self.p_rounds.exists() and self.d_rounds.exists():
-            queryset = [round for round in self.p_rounds.all()] + [round for round in self.d_rounds.all()]
+            queryset = [round for round in self.p_rounds.all()] + \
+                [round for round in self.d_rounds.all()]
             return sorted(queryset, key=lambda x: x.pairing.round_num)
         elif self.p_rounds.exists():
             return self.p_rounds.order_by('pairing__round_num').all()
@@ -48,8 +51,8 @@ class Team(models.Model):
             return self.d_rounds.order_by('pairing__round_num').all()
         else:
             return None
-        
-    def round_opponent(self, round_num): 
+
+    def round_opponent(self, round_num):
         p_rounds = self.p_rounds.filter(pairing__round_num=round_num)
         d_rounds = self.d_rounds.filter(pairing__round_num=round_num)
         if p_rounds.exists():
@@ -83,12 +86,12 @@ class Team(models.Model):
         if self.p_rounds.count() > 0:
             if self.user.tournament.judges == 1:
                 self.p_ballots = sum([ballot.p_ballot for round in self.p_rounds.all()
-                            for ballot in round.ballots.all() if ballot.judge == round.presiding_judge])
+                                      for ballot in round.ballots.all() if ballot.judge == round.presiding_judge])
             else:
                 self.p_ballots = sum([ballot.p_ballot for round in self.p_rounds.all()
-                        for ballot in round.ballots.all() if self.user.tournament.judges == 3  or \
-                            ballot.judge != round.extra_judge
-                        and ballot.round.pairing.round_num != 5])
+                                      for ballot in round.ballots.all() if self.user.tournament.judges == 3 or
+                                      ballot.judge != round.extra_judge
+                                      and ballot.round.pairing.round_num != 5])
         else:
             self.p_ballots = 0
 
@@ -96,12 +99,12 @@ class Team(models.Model):
         if self.d_rounds.count() > 0:
             if self.user.tournament.judges == 1:
                 self.d_ballots = sum([ballot.d_ballot for round in self.d_rounds.all()
-                        for ballot in round.ballots.all() if ballot.judge == round.presiding_judge])
+                                      for ballot in round.ballots.all() if ballot.judge == round.presiding_judge])
             else:
                 self.d_ballots = sum([ballot.d_ballot for round in self.d_rounds.all()
-                        for ballot in round.ballots.all() if self.user.tournament.judges == 3  or \
-                            ballot.judge != round.extra_judge
-                        and ballot.round.pairing.round_num != 5])
+                                      for ballot in round.ballots.all() if self.user.tournament.judges == 3 or
+                                      ballot.judge != round.extra_judge
+                                      and ballot.round.pairing.round_num != 5])
         else:
             self.d_ballots = 0
 
@@ -110,38 +113,39 @@ class Team(models.Model):
         self.total_ballots = self.p_ballots + self.d_ballots
 
     def calc_total_cs(self):
-        self.total_cs = sum([opponent.total_ballots for opponent in self.opponents()])
+        self.total_cs = sum(
+            [opponent.total_ballots for opponent in self.opponents()])
 
     def calc_total_pd(self):
         if self.d_rounds.count() > 0 or self.p_rounds.count() > 0:
             p_pd = sum([ballot.p_pd for round in self.p_rounds.all()
                         for ballot in round.ballots.all()
-                        if self.user.tournament.judges == 3 or \
+                        if self.user.tournament.judges == 3 or
                         (self.user.tournament.judges == 2 and
-                         ballot.judge != round.extra_judge) or \
+                         ballot.judge != round.extra_judge) or
                         (self.user.tournament.judges == 1 and
                          ballot.judge == round.presiding_judge)
                         and ballot.round.pairing.round_num != 5])
             d_pd = sum([ballot.d_pd for round in self.d_rounds.all()
                         for ballot in round.ballots.all()
-                        if self.user.tournament.judges == 3  or \
-                            (self.user.tournament.judges == 2 and
-                            ballot.judge != round.extra_judge) or \
-                            (self.user.tournament.judges == 1 and
-                            ballot.judge == round.presiding_judge)
+                        if self.user.tournament.judges == 3 or
+                        (self.user.tournament.judges == 2 and
+                         ballot.judge != round.extra_judge) or
+                        (self.user.tournament.judges == 1 and
+                         ballot.judge == round.presiding_judge)
                         and ballot.round.pairing.round_num != 5])
             self.total_pd = p_pd + d_pd
         else:
             self.total_pd = 0
-    
+
     def calc_spirit_score(self):
         spirit_score = 0
-        for i in range(4): 
+        for i in range(3):
             opponent = self.round_opponent(i+1)
-            if opponent and opponent.spirit and opponent.spirit.submit: 
-                spirit_score += opponent.spirit.get_score(i+1) 
+            if opponent and opponent.spirit and opponent.spirit.submit:
+                spirit_score += opponent.spirit.get_score(i+1)
         self.spirit_score = spirit_score
-    
+
     def current_rounds(self):
         return self.p_rounds.count() + self.d_rounds.count()
 
@@ -170,7 +174,6 @@ class Team(models.Model):
             competitor.save()
         super().save(*args, **kwargs)
 
-
     # def __lt__(self, other):
     #     if self.total_ballots() == other.total_ballots():
     #         if self.total_cs() == other.total_cs():
@@ -179,7 +182,3 @@ class Team(models.Model):
     #             return self.total_cs() - other.total_cs()
     #     else:
     #         return self.total_ballots() - other.total_ballots()
-
-
-
-
